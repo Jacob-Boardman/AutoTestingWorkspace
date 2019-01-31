6ab16051f2daa693d096a26e256e18f3ad53c928
Your Name <jacob.baordman.jb@gmail.com>
2019-01-31 10:36:44 +0000

    Cucumber

--- ExcelReadWrite/src/test/java/ExcelPara.java
+++ ExcelReadWrite/src/test/java/ExcelPara.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.*;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -22,7 +24,7 @@ import org.openqa.selenium.support.PageFactory;
 
 @RunWith(Parameterized.class)
 public class ExcelPara {
-	
+
 	WebDriver driver;
 
 	@Before
@@ -42,13 +44,20 @@ public class ExcelPara {
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
 		XSSFSheet sheet = workbook.getSheetAt(0);
 
-		Object[][] ob = new Object[sheet.getPhysicalNumberOfRows()-1][4];
+		Object[][] ob = new Object[sheet.getPhysicalNumberOfRows() - 1][4];
+
+		// for (int rowNum = 1; rowNum < sheet.getPhysicalNumberOfRows(); rowNum++) {
+		// ob[rowNum-1][0] = sheet.getRow(rowNum).getCell(0).getStringCellValue();
+		// ob[rowNum-1][1] = sheet.getRow(rowNum).getCell(1).getStringCellValue();
+		// ob[rowNum-1][2] = sheet.getRow(rowNum).getCell(2).getStringCellValue();
+		// ob[rowNum-1][3] = rowNum;
+		// }
 
 		for (int rowNum = 1; rowNum < sheet.getPhysicalNumberOfRows(); rowNum++) {
-			for (int colNum = 0; colNum < sheet.getRow(rowNum).getPhysicalNumberOfCells()-1; colNum++) {
-				ob[rowNum-1][colNum] = sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
+			for (int colNum = 0; colNum < sheet.getRow(rowNum).getPhysicalNumberOfCells(); colNum++) {
+				ob[rowNum - 1][colNum] = sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
 			}
-			ob[rowNum-1][3] = rowNum;
+			ob[rowNum - 1][3] = rowNum;
 		}
 
 		file.close();
@@ -66,60 +75,55 @@ public class ExcelPara {
 		this.expected = expected;
 		this.row = row;
 	}
-	
+
 	@Test
 	public void login() throws IOException {
-		
-		System.out.println(username +" "+ password + " " + expected + " " + row);
-		
+
+		System.out.println(username + " " + password + " " + expected + " " + row);
+
 		driver.manage().window().maximize();
 		driver.get("http://thedemosite.co.uk/addauser.php");
-		
+
 		ExcelDemoLandingPage edlp = PageFactory.initElements(driver, ExcelDemoLandingPage.class);
-		
+
 		edlp.createAndLogin(username, password);
 		WebElement login = driver.findElement(By.xpath(Constants.login));
 		login.click();
 		edlp.createAndLogin(username, password);
-		
+
 		FileInputStream file = new FileInputStream(Constants.excelpath);
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
 		XSSFSheet sheet = workbook.getSheetAt(0);
-		
+
 		XSSFRow rowy = sheet.getRow(row);
 		XSSFCell cellActual = rowy.getCell(3);
-		XSSFCell cellResult = rowy.getCell(4);
 		if (cellActual == null) {
 			cellActual = rowy.createCell(3);
 		}
+
 		WebElement text = driver.findElement(By.xpath(Constants.text));
 		cellActual.setCellValue(text.getText());
-		
-		if(cellResult == null) {
+
+		XSSFCell cellResult = rowy.getCell(4);
+		if (cellResult == null) {
 			cellResult = rowy.createCell(4);
 		}
-			
-		if(expected.equals(text.getText())) {
-			cellResult.setCellValue("Pass");								
-		}
-		else {
+		try {
+			assertEquals("Login not successful", expected, text.getText());
+
+			cellResult.setCellValue("Pass");
+
+		} catch (AssertionError e) {
+
 			cellResult.setCellValue("Fail");
+		} finally {
+
+			FileOutputStream fileOut = new FileOutputStream(Constants.excelpath);
+
+			workbook.write(fileOut);
+			fileOut.flush();
+			file.close();
 		}
-		
-		
-		WebElement adduser = driver.findElement(By.xpath(Constants.adduser));
-		adduser.click();
-
-	
-	
-	FileOutputStream fileOut = new FileOutputStream(Constants.excelpath);
-	
-	workbook.write(fileOut);
-	fileOut.flush();
-	file.close();
-		
-		
 	}
-
 
 }
